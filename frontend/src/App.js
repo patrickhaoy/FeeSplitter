10614572e0f9184cc2058c166b417dedde68c598
hg1722 <henrygu1722@gmail.com>
2019-08-05 17:02:02 -0700

    added transaction capability

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import logo from './logo.svg';
 import './App.css';
-import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Checkbox from '@material-ui/core/Checkbox';
-import Avatar from '@material-ui/core/Avatar';
-import UserListSecondary from './UserListSecondary'
+import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 class UsersView extends React.Component {
   constructor(props) {
@@ -28,7 +31,7 @@ class UsersView extends React.Component {
   }
 
   getUsers() {
-    fetch('https://fee-splitter.herokuapp.com')
+    fetch('https://fee-splitter.herokuapp.com/users')
       .then(response => response.json())
       .then(response =>
         this.setState(
@@ -39,7 +42,7 @@ class UsersView extends React.Component {
 
   renderUsers = ({ user_id, firstName }) => <div key={user_id}>{firstName}</div>
 
-  handleToggle(){
+  handleToggle() {
     console.log('toggled');
   }
 
@@ -47,32 +50,112 @@ class UsersView extends React.Component {
 
   render() {
     return (
-      <div className="Users">
-        {this.state.users.map(this.renderUsers)}
-        <List>
-          {this.state.users.map(user => {
-            const labelId = `checkbox-list-secondary-label-${user.firstName}`;
-            return (
-              <ListItem key={user} button>
-                <ListItemAvatar>
-                  <Avatar
-                    alt={`Avatar n°${user.user_id}`}
-                    src={`/static/images/avatar/${user.user_id + 1}.jpg`}
-                  />
-                </ListItemAvatar>
-                <ListItemText id={labelId} primary={user.firstName} />
-                <ListItemSecondaryAction>
-                  <Checkbox
-                    edge="end"
-                    onChange={this.handleToggle(user.user_id)}
-                    //checked={checked.indexOf(value) !== -1}
-                    inputProps={{ 'aria-labelledby': labelId }}
-                  />
-                </ListItemSecondaryAction>
-              </ListItem>
-            );
-          })}
-        </List>
+      <div style={{ width: '20%', margin: '1em', padding: '0.5em' }} className="Users">
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" className="IconButton" color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography style={{ flex: 1 }} variant="h6" className="Title">
+              Users
+          </Typography>
+            <Button color="inherit">Add</Button>
+          </Toolbar>
+        </AppBar>
+        <Paper>
+          <List>
+            {this.state.users.map(user => {
+              const labelId = `checkbox-list-secondary-label-${user.firstName}`;
+              return (
+                <ListItem key={user} button>
+                  <ListItemText id={labelId} primary={user.firstName} />
+                  <ListItemSecondaryAction>
+                    <Checkbox
+                      edge="end"
+                      onChange={this.handleToggle(user.user_id)}
+                      //checked={checked.indexOf(value) !== -1}
+                      inputProps={{ 'aria-labelledby': labelId }}
+                    />
+                  </ListItemSecondaryAction>
+                </ListItem>
+              );
+            })}
+          </List>
+        </Paper>
+      </div>
+    );
+  }
+}
+
+class TransactionsView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      transactions: []
+    }
+    this.getTransactions = this.getTransactions.bind(this);
+    this.renderTransactions = this.renderTransactions.bind(this);
+    this.handleToggle = this.handleToggle.bind(this);
+  }
+
+  componentDidMount() {
+    this.getTransactions();
+  }
+
+  getTransactions() {
+    fetch('https://fee-splitter.herokuapp.com/transactions')
+      .then(response => response.json())
+      .then(response =>
+        this.setState(
+          {
+            transactions: response.data
+          }))
+  }
+
+  renderTransactions = ({ title, amount }) => <div key={title}>{amount}</div>
+
+  handleToggle() {
+    console.log('toggled');
+  }
+
+  //[checked, setChecked] = React.useState([1]);
+
+  render() {
+    return (
+      <div style={{ width: '20%', margin: '1em', padding: '0.5em' }} className="Transactions">
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" className="IconButton" color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography style={{ flex: 1 }} variant="h6" className="Title">
+              Transactions
+          </Typography>
+            <Button color="inherit">Add</Button>
+          </Toolbar>
+        </AppBar>
+        <Paper>
+          <List>
+            {this.state.transactions.map(transaction => {
+              const labelId = `checkbox-list-secondary-label-${transaction.title}`;
+              const labelAmount = `checkbox-list-secondary-label-${transaction.amount}`;
+              return (
+                <ListItem key={transaction} button>
+                  <ListItemText id={labelId} primary={transaction.title} />
+                  <ListItemText id={labelAmount} primary={transaction.amount} />
+                  <ListItemSecondaryAction>
+                    <Checkbox
+                      edge="end"
+                      onChange={this.handleToggle(transaction.tranID)}
+                      //checked={checked.indexOf(value) !== -1}
+                      inputProps={{ 'aria-labelledby': labelId }}
+                    />
+                  </ListItemSecondaryAction>
+                </ListItem>
+              );
+            })}
+          </List>
+        </Paper>
       </div>
     );
   }
@@ -87,6 +170,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <UsersView></UsersView>
+        <TransactionsView></TransactionsView>
       </div>
     );
   }
