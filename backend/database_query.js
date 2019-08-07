20213d2e845768333fc3324113e80f56acaca4f9
Patrick Yin <patarhaoy@gmail.com>
2019-08-06 23:37:15 -0700

    Groups table

--- backend/database_query.js
+++ backend/database_query.js
@@ -7,26 +7,49 @@ var con = mysql.createConnection({
 	database: "feesplitter"
 });
 
-con.connect(function(err) {	
-	//owes(1, 2);
+con.connect(function(err) {
+	//console.log(owes(1, 2));
+	console.log(netOwes(1, 2));
+	//netOwes(1, 2);
 });
+/*function owes (user1, user2, ) {
+	var sqlPay = "SELECT SUM(amount) AS sum FROM transactions WHERE fromID = ? and toID = ?";
+	const pay = await con.query(sqlPay, [user1, user2], function (err, result) {
+		if (err) callback(false);
+		else return callback(result[0].sum);
+	});
+}*/
 
-function owes (user1, user2) {
+function netOwes (user1, user2) {
 	var sqlPay = "SELECT SUM(amount) AS sum FROM transactions WHERE fromID = ? and toID = ?";
 	var pay, receive;
-	
 	con.query(sqlPay, [user1, user2], function (err, result) {
+		if (err) throw err;
+		pay = result;
+		con.query(sqlPay, [user2, user1], function (err, result) {
+			if (err) throw err;
+			receive = result; 
+			return ((pay[0].sum - receive[0].sum).toFixed(2));
+		});
+	});
+
+	/*con.query(sqlPay, [user1, user2], function (err, result) {
 		if (err) throw err;
 		pay = result
 		console.log(pay[0].sum);
+		//console.log("2");
 	});
-
+	//console.log("3");
 	con.query(sqlPay, [user2, user1], function (err, result) {
 		if (err) throw err;
 		receive = result;
 		console.log(receive[0].sum);
-		console.log((pay[0].sum - receive[0].sum).toFixed(2));
+		//console.log((pay[0].sum - receive[0].sum).toFixed(2));
+		//console.log("4");
 	});
+	//console.log("5");
+	console.log(pay[0].sum + " " + receive[0].sum);
+	//return ((pay[0].sum - receive[0].sum).toFixed(2));*/
 }
 
 function findUserID (firstName, lastName) {
