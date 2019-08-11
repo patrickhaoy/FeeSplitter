ea5cbb5a914c6bba8c59f452d0b1401a3787ccd3
Patrick Yin <patrickhaoy@Patricks-MacBook-Pro.local>
2019-08-10 19:42:19 -0700

    new app.get functions

--- backend/database_query.js
+++ backend/database_query.js
@@ -8,28 +8,11 @@ var con = mysql.createConnection({
 });
 
 con.connect(function(err) {
-	const sqlPay = "SELECT sum(amount) as sum FROM transactions JOIN groups ON transactions.groupID = groups.groupID WHERE transactions.groupID = ? and fromID = ? and toID = ?";
-	var pay, receive;
-	con.query(sqlPay, [1, 1, 2], function (err, result) {
+	const sql = "SELECT * FROM userGroups JOIN groups ON groups.groupID = userGroups.groupID WHERE groups.groupID = ?";
+	con.query(sql, [1], function (err, result) {
 		if (err) console.log(err);
 		else {
-			pay = result;
-			con.query(sqlPay, [1, 2, 1], function (err, result) {
-				if (err) console.log(err);
-				else {
-					receive = result; 
-					const owe = (pay[0].sum - receive[0].sum).toFixed(2);
-					if (owe >= 0) {
-						console.log({
-							data: [{"groupID":1, "fromID":1, "toID":2, "owedAmount":owe}]
-						})
-					} else {
-						console.log({
-						data: [{"groupID":1, "fromID":2, "toID":1, "owedAmount":-owe}]
-					})
-					}
-				}
-			});
+			console.log(result);
 		}
 	});
 });
--- server.js
+++ server.js
@@ -23,11 +23,53 @@ con.connect(err => {
  
 app.use(cors());
 
+// returns user info for "userID"
+app.get('/users', (req, res) => {
+	const { userID } = req.query;
+	const sql = "SELECT * FROM users WHERE userID = ?";
+	con.query(sql, [userID], function (err, result) {
+		if (err) res.send(err);
+		else {
+			return res.json({
+				data: result
+			})
+		}
+	})
+});
+
+// returns transaction info for "tranID"
+app.get('/transactions', (req, res) => {
+	const { tranID } = req.query;
+	const sql = "SELECT * FROM transactions WHERE tranID = ?";
+	con.query(sql, [tranID], function (err, result) {
+		if (err) res.send(err);
+		else {
+			return res.json({
+				data: result
+			})
+		}
+	})
+});
+
 // returns groups that "userID" is in
-app.get('/groups', (req, res) => {
+app.get('/groups/users', (req, res) => {
 	const {userID} = req.query;
 	const sql = "SELECT * FROM userGroups JOIN groups JOIN users ON groups.groupID = userGroups.groupID AND userGroups.userID = users.userID WHERE userGroups.userID = ?";
 	con.query(sql, [userID], function (err, result) {
+		if (err) res.send(err);
+		else {
+			return res.json({
+				data: result
+			})
+		}
+	})
+})
+
+// returns users in "groupID"
+app.get('/users/groups', (req, res) => {
+	const {groupID} = req.query;
+	const sql = "SELECT * FROM userGroups JOIN groups ON groups.groupID = userGroups.groupID WHERE groups.groupID = ?";
+	con.query(sql, [groupID], function (err, result) {
 		if (err) res.send(err);
 		else {
 			return res.json({
@@ -89,8 +131,6 @@ app.get('/groups/users/owe', (req, res) => {
 		}
 	});
 });
-
-
 
 /*Everything below returns table values*/
 
